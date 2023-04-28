72a3535d973326fc19ecc0100192774680ccdc25
rafael-gontijo <rafael-gontijo@outlook.com.br>
2023-04-28 00:58:02 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -9,6 +10,9 @@ public class Program {
 		System.out.println(position);
 		position.setValues(8, 10);
 		System.out.println(position);
+		
+		Board board = new Board(8,8);
+		
 	}
 
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,11 +2,11 @@ package boardgame;
 
 public class Position {
 	private int row;
-	private int collumn;
+	private int column;
 
-	public Position(int row, int collumn) {
+	public Position(int row, int column) {
 		this.row = row;
-		this.collumn = collumn;
+		this.column = column;
 	}
 
 	public int getRow() {
@@ -17,21 +17,21 @@ public class Position {
 		this.row = row;
 	}
 
-	public int getCollumn() {
-		return collumn;
+	public int getColumn() {
+		return column;
 	}
 
-	public void setCollumn(int collumn) {
-		this.collumn = collumn;
+	public void setColumn(int column) {
+		this.column = column;
 	}
 	
-	public void setValues(int row, int collumn) {
+	public void setValues(int row, int column) {
 		this.row = row;
-		this.collumn = collumn;
+		this.column = column;
 	}
 	
 	@Override
 	public String toString() {
-		return row + ", " + collumn;
+		return row + ", " + column;
 	}
 }
