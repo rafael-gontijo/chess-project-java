fc521b9f62ce7a0c3c0ca5566bd24f49ce5ae2be
rafael-gontijo <rafael-gontijo@outlook.com.br>
2023-05-07 01:54:42 -0300

    Pawn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -24,7 +24,6 @@ public class King extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-
 		Position p = new Position(0, 0);
 
 		// above
